dfe55344e3b82af4380ea077dbace3d6f1c9d0f2
marcoosavelino <71901248@aluno.faculdadecotemig.br>
2022-09-21 09:56:04 -0300

    Fundamentos

---
+++ Fundamentos/Fundamentos/Program.cs
@@ -0,0 +1,104 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Fundamentos;
+
+int[] arrayInteiros = new int[3];
+
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 50;
+
+Console.WriteLine("Percorrendo Array com FOR");
+
+for (int contador = 0; contador < arrayInteiros.Length; contador++)
+{
+    Console.WriteLine("Posição Nº " + contador);
+    Console.WriteLine(arrayInteiros[contador]);
+}
+
+int contadorForEach = 0;
+Console.WriteLine("Percorrendo Array com FOREACH");
+
+foreach (int valor in arrayInteiros)
+{
+    Console.WriteLine("Posição Nº " + contadorForEach);
+    Console.WriteLine(valor);
+    contadorForEach++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Pessoa pessoa1 = new Pessoa();
+pessoa1.Nome = "Marcos Miguel";
+pessoa1.Idade = 22;
+
+pessoa1.Apresentar();
+*/
+
+//DateTime dataAtual = DateTime.Now;
+//Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
+
+/* int a = 10;
+int b = 20;
+
+int c = a + b;
+
+Console.WriteLine(c);*/
+
+/*
+int quantidadeEmEstoque = 10;
+int quantidadeCompra = 4;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+
+Console.WriteLine("Quantidade em estoque: " + quantidadeEmEstoque);
+Console.WriteLine("Quantidade compra: " + quantidadeCompra);
+Console.WriteLine("É possível realizar a venda? " + possivelVenda);
+
+if (possivelVenda)
+{
+    Console.WriteLine("Venda realizada.");
+}
+else
+{
+    Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
+}*/
+
+/*
+Console.WriteLine("Digite uma letra: ");
+ConsoleKeyInfo letra = Console.ReadKey();
+
+
+
+switch (letra.Key)
+{
+    case ConsoleKey.A:
+        Console.WriteLine("Vogal");
+        break;
+     case ConsoleKey.E:
+        Console.WriteLine("Vogal");
+        break;
+    case ConsoleKey.I:
+        Console.WriteLine("Vogal");
+        break;
+    case ConsoleKey.O: 
+        Console.WriteLine("Vogal");
+        break;
+    case ConsoleKey.U:
+        Console.WriteLine("Vogal");
+        break;
+    default:
+        Console.WriteLine("Não é uma vogal");
+        break;
+}
+*/
